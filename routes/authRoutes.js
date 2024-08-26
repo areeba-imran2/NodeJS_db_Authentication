ff613273d2a7eafae7b1d7ddaef8fc24f0e48e7c
areeba-imran2 <areebaimran.edu@gmail.com>
2024-08-26 18:50:18 +0500

    Update Routes Folder

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const { signup, login } = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/signup', signup);  // Note that this is a POST route
+router.post('/signup', signup);
 router.post('/login', login);
 
 module.exports = router;

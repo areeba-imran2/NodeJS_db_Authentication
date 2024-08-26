fa14f70730aee814949d14b9520850873b98c163
areeba-imran2 <areebaimran.edu@gmail.com>
2024-08-26 18:49:45 +0500

    Update the Code in index.js

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/authRoutes');  // Import auth routes
-const protectedRoutes = require('./routes/protectedRoutes');  // Import protected routes
-const db = require('./models');  // Import models
+const authRoutes = require('./routes/authRoutes'); 
+const protectedRoutes = require('./routes/protectedRoutes');  
+const db = require('./models'); 
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());  // Middleware to parse JSON
-app.use(cookieParser());  // Middleware to parse cookies
+ // Middleware to parse JSON
+app.use(express.json()); 
+// Middleware to parse cookies
+app.use(cookieParser());
 
-app.use('/auth', authRoutes);  // Register auth routes under /auth path
-app.use('/protected', protectedRoutes);  // Register protected routes under /protected path
+app.use('/auth', authRoutes);  
+app.use('/protected', protectedRoutes); 
 
-// Sync Sequelize models with the database and start the server
 db.sequelize.sync().then(() => {
     app.listen(3000, () => console.log('Server running on port 3000'));
 });
